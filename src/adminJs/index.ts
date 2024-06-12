f2d3825f1eaa750504656ee7e12db85362fe6def
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-06-12 15:57:01 -0300

    Primeiro Resource

--- src/adminJs/index.ts
+++ src/adminJs/index.ts
@@ -2,6 +2,7 @@ import AdminJs from 'adminjs'
 import AdminJsExpress from '@adminjs/express'
 import AdminJsSequelize from '@adminjs/sequelize'
 import { database } from '../database'
+import { adminJsResources } from './resources'
 
 
 AdminJs.registerAdapter(AdminJsSequelize)
@@ -9,6 +10,7 @@ AdminJs.registerAdapter(AdminJsSequelize)
 export const adminJs = new AdminJs({
     databases: [database],
     rootPath: '/admin',
+    resources: adminJsResources,
     branding: {
         companyName: 'OneBitFlix',
         logo: '/logoOnebitflix.svg',
