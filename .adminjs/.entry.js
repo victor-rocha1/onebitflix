14c249aa3e7ec872f6a0aff6fd6799778d6a0c50
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-06-13 17:00:26 -0300

    Personalização do Painel Admin

--- .adminjs/.entry.js
+++ .adminjs/.entry.js
@@ -5,3 +5,5 @@ import Component2 from '../node_modules/@adminjs/upload/src/features/upload-file
 AdminJS.UserComponents.Component2 = Component2
 import Component3 from '../node_modules/@adminjs/upload/src/features/upload-file/components/show'
 AdminJS.UserComponents.Component3 = Component3
+import Component4 from '../src/adminJs/components/Dashboard'
+AdminJS.UserComponents.Component4 = Component4
--- src/adminJs/index.ts
+++ src/adminJs/index.ts
@@ -3,9 +3,10 @@ import AdminJsExpress from '@adminjs/express'
 import AdminJsSequelize from '@adminjs/sequelize'
 import { database } from '../database'
 import { adminJsResources } from './resources'
-import { User } from '../models'
+import { Category, Course, Episode, User } from '../models'
 import bcrypt from 'bcrypt'
 import { locale } from './locale'
+import AdminJS from 'adminjs'
 
 
 AdminJs.registerAdapter(AdminJsSequelize)
@@ -35,7 +36,24 @@ export const adminJs = new AdminJs({
             }
         }
     },
-    locale: locale
+    locale: locale,
+    dashboard: {
+        component: AdminJS.bundle("./components/Dashboard"),
+        handler: async (req, res, context) => {
+            const courses = await Course.count()
+            const episodes = await Episode.count()
+            const category = await Category.count()
+            const standardUsers = await User.count({ where: { role: 'user' } })
+            
+            res.json({
+                'Cursos': courses,
+                'Episódios': episodes,
+                'Categorias': category,
+                'Usuários': standardUsers
+            })
+        }
+    }
+
 })
 
 export const adminJsRouter = AdminJsExpress.buildAuthenticatedRouter(adminJs, {
