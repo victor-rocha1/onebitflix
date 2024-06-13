4e5afa2915dc73b6caee1da3b343bf54a58c5c29
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-06-13 17:46:52 -0300

    Organizando o código do AdminJs

--- .adminjs/.entry.js
+++ .adminjs/.entry.js
@@ -1,9 +1,9 @@
 AdminJS.UserComponents = {}
-import Component1 from '../node_modules/@adminjs/upload/src/features/upload-file/components/edit'
+import Component1 from '../src/adminjs/components/Dashboard'
 AdminJS.UserComponents.Component1 = Component1
-import Component2 from '../node_modules/@adminjs/upload/src/features/upload-file/components/list'
+import Component2 from '../node_modules/@adminjs/upload/src/features/upload-file/components/edit'
 AdminJS.UserComponents.Component2 = Component2
-import Component3 from '../node_modules/@adminjs/upload/src/features/upload-file/components/show'
+import Component3 from '../node_modules/@adminjs/upload/src/features/upload-file/components/list'
 AdminJS.UserComponents.Component3 = Component3
-import Component4 from '../src/adminJs/components/Dashboard'
+import Component4 from '../node_modules/@adminjs/upload/src/features/upload-file/components/show'
 AdminJS.UserComponents.Component4 = Component4
--- src/adminJs/index.ts
+++ src/adminJs/index.ts
@@ -1,77 +1,27 @@
-import AdminJs from 'adminjs'
-import AdminJsExpress from '@adminjs/express'
-import AdminJsSequelize from '@adminjs/sequelize'
-import { database } from '../database'
+import { dashboardOptions } from './dashboard'
+import { brandingOptions } from './branding'
+import { authtenticationOptions } from './authentication'
 import { adminJsResources } from './resources'
-import { Category, Course, Episode, User } from '../models'
-import bcrypt from 'bcrypt'
-import { locale } from './locale'
+import { database } from '../database'
 import AdminJS from 'adminjs'
+import AdminJSExpress from '@adminjs/express'
+import AdminJSSequelize from '@adminjs/sequelize'
+import { locale } from './locale'
 
+AdminJS.registerAdapter(AdminJSSequelize)
 
-AdminJs.registerAdapter(AdminJsSequelize)
-
-export const adminJs = new AdminJs({
+export const adminJs = new AdminJS({
     databases: [database],
-    rootPath: '/admin',
     resources: adminJsResources,
-    branding: {
-        companyName: 'OneBitFlix',
-        logo: '/logoOnebitflix.svg',
-        theme: {
-            colors: {
-                primary100: '#ff0043',
-                primary80: '#ff1a57',
-                primary60: '#ff3369',
-                primary40: '#ff4d7c',
-                primary20: '#ff668f',
-                grey100: '#151515',
-                grey80: '#333333',
-                grey60: '#4d4d4d',
-                grey40: '#666666',
-                grey20: '#dddddd',
-                filterBg: '#333333',
-                accent: '#151515',
-                hoverBg: '#151515',
-            }
-        }
-    },
+    rootPath: '/admin',
+    dashboard: dashboardOptions,
     locale: locale,
-    dashboard: {
-        component: AdminJS.bundle("./components/Dashboard"),
-        handler: async (req, res, context) => {
-            const courses = await Course.count()
-            const episodes = await Episode.count()
-            const category = await Category.count()
-            const standardUsers = await User.count({ where: { role: 'user' } })
-            
-            res.json({
-                'Cursos': courses,
-                'Episódios': episodes,
-                'Categorias': category,
-                'Usuários': standardUsers
-            })
-        }
-    }
-
+    branding: brandingOptions
 })
 
-export const adminJsRouter = AdminJsExpress.buildAuthenticatedRouter(adminJs, {
-    authenticate: async (email, password) => {
-        const user = await User.findOne({ where: { email } })
-
-        if (user && user.role === 'admin') {
-            const matched = await bcrypt.compare(password, user.password)
-
-            if (matched) {
-                return user
-            }
-        }
-
-        return false
-    },
-    cookiePassword: 'senha-do-cookie'
-}, null, {
-    resave: false,
-    saveUninitialized: false
-})
+export const adminJsRouter = AdminJSExpress.buildAuthenticatedRouter(
+    adminJs,
+    authtenticationOptions,
+    null,
+    { resave: false, saveUninitialized: false }
+)
