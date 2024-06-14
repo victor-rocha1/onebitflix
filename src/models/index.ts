4c5222c46a88dcf6de9b99e96e90b6d1fa4c6800
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-06-14 15:37:25 -0300

    Filtrando os 3 cursos em destaque

--- src/models/index.ts
+++ src/models/index.ts
@@ -6,7 +6,7 @@ import { User } from './User'
 Category.hasMany(Course, { as: 'courses' })
 
 Course.belongsTo(Category)
-Course.hasMany(Episode) //Episode
+Course.hasMany(Episode, { as: 'episodes' }) //Episode
 
 Episode.belongsTo(Course)
 
