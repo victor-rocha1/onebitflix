434303f1043d0b792057bc1f81e29897b4127ac3
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-06-17 15:04:10 -0300

    Streaming de Vídeo na API

---
+++ src/controllers/episodesController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express'
+import { episodeService } from '../services/episodeServices';
+
+export const episodesController = {
+  //Define qual parte do vídeo vai ser carregada
+  stream: async (req: Request, res: Response) => {
+    const { videoUrl } = req.query
+    const range = req.headers.range
+
+    try {
+      if (typeof videoUrl !== 'string') {
+        throw new Error('videoUrl must be of type \'string\'');
+      }
+
+      episodeService.streamEpisodeToResponse(res, videoUrl, range)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+}
+
