ab9b8a80b80d6a23442f01b1ef332fc566a1b4f5
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-06-18 14:52:43 -0300

    Obtendo os cursos Favoritos

--- src/services/favoriteService.ts
+++ src/services/favoriteService.ts
@@ -1,12 +1,33 @@
-import { Favorite } from "../models/Favorite"
+import { Favorite } from "../models"
 
 export const favoriteService = {
-    create: async (userId: number, courseId: number) => {
-        const favorite = await Favorite.create({
-            userId,
-            courseId
+    findByUserId: async (userId: number) => {
+        const favorites = await Favorite.findAll({
+            attributes: [['user_id', 'userId']],
+            where: { userId },
+            include: {
+                association: 'Course',
+                attributes: [
+                    'id',
+                    'name',
+                    'synopsis',
+                    ['thumbnail_url', 'thumbnailUrl']
+                ]
+            }
         })
 
-        return favorite
+        return {
+            userId,
+            courses: favorites.map(favorite => favorite.Course)
+        }
     },
+    create: async (userId: number, courseId: number) => {
+        try {
+            await Favorite.create({ userId, courseId })
+            return true
+        } catch (error) {
+            console.error(error)
+            return false
+        }
+    }
 }
