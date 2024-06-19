0a494c771f26bc68c5df0b1b39cc3bb6dd0ccb10
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-06-19 17:31:28 -0300

    Configuração Inicial do Front-End

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import '../../styles/HomenoAuth.scss'
+
+function HomenoAuth() {
+
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+			<main></main>
+    </>
+  )
+}
+
+export default HomenoAuth
