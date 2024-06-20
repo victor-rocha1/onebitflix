b4ca7362081afcb776a5e856dce7df83010604eb
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-06-20 15:11:38 -0300

    Ajuste nos estilos backgroud

--- frontend/src/components/headernoAuth/index.tsx
+++ frontend/src/components/headernoAuth/index.tsx
@@ -1,10 +1,12 @@
 import { Container, Button } from "reactstrap";
 import styles from "./styles.module.scss";
+import Link from "next/link";
 
 
 const HeaderNoAuth = function () {
     return (
         <>
+        <main>
             <div className={styles.ctaSection}>
                 <img src="/homeNoAuth/logoCta.png" alt="logoCta" className={styles.imgCta} />
                 <p>Se cadastre para ter acesso aos cursos</p>
@@ -14,10 +16,15 @@ const HeaderNoAuth = function () {
             <Container className={styles.nav}>
                 <img src="/logoOnebitflix.svg" alt="logoOnebitflix" className={styles.imgLogoNav} />
                 <div>
+                    <Link href='/login'>
                         <Button className={styles.navBtn} outline>Entrar</Button>
+                    </Link>
+                    <Link href='/registro'>
                         <Button className={styles.navBtn} outline>Quero fazer parte</Button>
+                    </Link>
                 </div>
             </Container>
+        </main>
         </>
     );
 };
