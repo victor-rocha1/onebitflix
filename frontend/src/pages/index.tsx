ec0438ed4b4a52f7f90ed1e3f3df949a516d2712
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-06-21 14:55:29 -0300

    Usando o AOS

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,8 +4,10 @@ import HeaderNoAuth from '../components/homenoAuth/headernoAuth';
 import PresentationSection from '../components/homenoAuth/presentationsection';
 import SlideSection from '../components/homenoAuth/slideSection';
 import courseService, { CourseType } from '../services/courseService';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 import Footer from '../components/common/footer';
+import AOS from 'aos';
+import "aos/dist/aos.css";
 
 interface IndexPageProps {
   children?: ReactNode;
@@ -13,15 +15,24 @@ interface IndexPageProps {
 }
 
 function HomenoAuth({ course }: IndexPageProps) {
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <>
       <main>
-        <div className='sectionBackground'>
+        <div className='sectionBackground' data-aos="fade-zoom-in" data-aos-duration="1600">
           <HeaderNoAuth />
           <PresentationSection />
         </div>
-        <CardsSection />
-        <SlideSection newestCourses={course} />
+        <div data-aos="fade-right" data-aos-duration="1200">
+          <CardsSection />
+        </div>
+        <div data-aos="fade-up" data-aos-duration="1350">
+          <SlideSection newestCourses={course} />
+        </div>
         <Footer />
       </main>
     </>
