9fa29c24c44bea701a6ccebe2de185c32e06f225
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-06-25 15:51:48 -0300

    Slide destaque

--- frontend/pages/home.tsx
+++ frontend/pages/home.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import FeaturedSection from "../src/components/homeAuth/featuredSection";
 import NewestCategory from "../src/components/homeAuth/newestCategory";
 import FavoritesCourses from "../src/components/homeAuth/favoriteCategory";
+import FeaturedCategory from "../src/components/homeAuth/featuredCategory";
 
 const HomeAuth = function () {
     return (
@@ -14,6 +15,7 @@ const HomeAuth = function () {
                 <FeaturedSection />
                 <NewestCategory />
                 <FavoritesCourses />
+                <FeaturedCategory />
             </main>
         </>
     );
---
+++ frontend/src/components/homeAuth/featuredCategory/index.tsx
@@ -0,0 +1,21 @@
+import styles from "../../../../styles/slideCategory.module.scss";
+import useSWR from "swr";
+import courseService from "../../../services/courseService";
+import SlideComponent from "../../common/slideComponent";
+
+const FeaturedCategory = function () {
+    const { data, error } = useSWR("/featured", courseService.getFeaturedCourses);
+
+    if (error) return error;
+
+    return(
+        <>
+        <p className={styles.titleCategory}>EM DESTAQUE</p>
+        <SlideComponent course={data.data} />
+        </>
+    )
+}
+
+
+
+export default FeaturedCategory;
