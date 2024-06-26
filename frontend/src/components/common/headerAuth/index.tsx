48977bca73f1cbf76c01fa67ec7a0a6014fd66e0
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-06-26 16:35:01 -0300

    Conexão do backend relacionado aos dados do usuário.
    Conexão do backend relacionado aos dados do usuário.
    Criação de um serviço e também de um método e também de toda a parte de conexão com o front
    Conexão backend dados do usuário.

--- frontend/src/components/common/headerAuth/index.tsx
+++ frontend/src/components/common/headerAuth/index.tsx
@@ -2,8 +2,19 @@ import Link from "next/link";
 import styles from "./styles.module.scss";
 import { Container, Input, Form } from "reactstrap";
 import Modal from "react-modal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import profileService from "../../../services/profileService";
+
+const [initials, setInitials] = useState("");
+
+useEffect(() => {
+    profileService.fetchCurrent().then((user) => {
+        const firstNameInitial = user.firstName.slice(0, 1);
+        const lastNameInitial = user.lastName.slice(0, 1);
+        setInitials(firstNameInitial + lastNameInitial);
+    });
+}, []);
 
 Modal.setAppElement("#__next");  //Para deficientes visuais
 
@@ -43,20 +54,21 @@ const HeaderAuth = function () {
                     alt="lupaHeader"
                     className={styles.searchImg}
                 />
-                <p className={styles.userProfile} onClick={handleOpenModal}>AB</p>
-            </div>
-            <Modal
-                isOpen={modalOpen}
-                onRequestClose={handleCloseModal}
-                shouldCloseOnEsc={true}
-                className={styles.modal}
-                overlayClassName={styles.overlayModal}
-            >
-                <Link href="/profile">
-                    <p className={styles.modalLink}>Meus Dados</p>
-                </Link>
-                <p className={styles.modalLink} onClick={handleLogout}>Sair</p>
-            </Modal>
+                <p className={styles.userProfile} onClick={handleOpenModal}>
+                    {initials}
+                </p>
+                <Modal
+                    isOpen={modalOpen}
+                    onRequestClose={handleCloseModal}
+                    shouldCloseOnEsc={true}
+                    className={styles.modal}
+                    overlayClassName={styles.overlayModal}
+                >
+                    <Link href="/profile">
+                        <p className={styles.modalLink}>Meus Dados</p>
+                    </Link>
+                    <p className={styles.modalLink} onClick={handleLogout}>Sair</p>
+                </Modal>
         </Container>
     );
 };
