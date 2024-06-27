d5378a9773d228fe0ce1593c4e023816a80e89fb
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-06-27 16:56:04 -0300

    Página principal de Curso

--- frontend/src/services/courseService.ts
+++ frontend/src/services/courseService.ts
@@ -61,6 +61,22 @@ const courseService = {
         return res;
     },
 
+    removeFav: async (courseId: number | string) => {
+        const token = sessionStorage.getItem("onebitflix-token");
+        const res = await api.delete("/favorites", {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+            data: { courseId },
+        }).catch((error) => {
+            console.log(error.response.data.message);
+
+            return error.response;
+        });
+
+        return res;
+    },
+
     getFavCourses: async () => {
         const token = sessionStorage.getItem("onebitflix-token");
 
@@ -69,6 +85,47 @@ const courseService = {
                 headers: {
                     Authorization: `Bearer ${token}`,
                 },
+            })
+            .catch((error) => {
+                console.log(error.response.data.message);
+
+                return error.response;
+            });
+
+        return res;
+    },
+
+    like: async (courseId: number | string) => {
+        const token = sessionStorage.getItem("onebitflix-token");
+
+        const res = await api
+            .post(
+                "likes",
+                { courseId },
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            )
+            .catch((error) => {
+                console.log(error.response.data.message);
+
+                return error.response;
+            });
+
+        return res;
+    },
+
+    removeLike: async (courseId: number | string) => {
+        const token = sessionStorage.getItem("onebitflix-token");
+
+        const res = await api
+            .delete("/likes", {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+                data: { courseId },
             })
             .catch((error) => {
                 console.log(error.response.data.message);
@@ -96,6 +153,24 @@ const courseService = {
 
         return res;
     },
+
+    getEpisodes: async (id: number | string) => {
+        const token = sessionStorage.getItem("onebitflix-token");
+
+        const res = await api
+            .get(`/courses/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+            .catch((error) => {
+                console.log(error.response.data.message);
+
+                return error.response;
+            });
+
+        return res;
+    },
 }
 
-export default courseService
+export default courseService;
