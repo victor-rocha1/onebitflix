4a591b71921decd46c827c84ffbe8e66b961b21e
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-06-27 15:29:41 -0300

    Criação da Página Search

--- frontend/pages/search.tsx
+++ frontend/pages/search.tsx
@@ -2,14 +2,16 @@ import styles from "../styles/search.module.scss";
 import Head from "next/head";
 import HeaderAuth from "../src/components/common/headerAuth";
 import { useRouter } from "next/router";
-import { useFormState } from "react-dom";
 import courseService, { CourseType } from "../src/services/courseService";
 import { useEffect, useState } from "react";
+import { Container } from "reactstrap";
+import SearchCard from "../src/components/searchCard";
+import Footer from "../src/components/common/footer";
 
 const Search = function () {
 
     const router = useRouter();
-    const searchName:any = router.query.name;
+    const searchName: any = router.query.name;
     const [searchResult, setSearchResult] = useState<CourseType[]>([]);
 
     const searchCourses = async function () {
@@ -27,16 +29,25 @@ const Search = function () {
     return (
         <>
             <Head>
-                <title>Onebitflix - {"searchName"}</title>
+                <title>Onebitflix - {searchName}</title>
                 <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
             </Head>
             <main>
-                <HeaderAuth />
-                {searchResult?.map((course) => (
-                    <div key={course.id}>
-                        <p>{course.name}</p>
-                    </div>
-                ))}
+                <div className={styles.header}>
+                    <HeaderAuth />
+                </div>
+                {searchResult.length >= 1 ? (
+                    <Container className="d-flex flex-wrap justify-content-center gap-5 py-4">
+                        {searchResult?.map((course) => (
+                            <SearchCard key={course.id} course={course} />
+                        ))}
+                    </Container>
+                ) : (
+                    <p className={styles.noSearchText}>Nenhum resultado encontrado!</p>
+                )}
+                <div className={styles.footer}>
+                    <Footer />
+                </div>
             </main>
         </>
     );
